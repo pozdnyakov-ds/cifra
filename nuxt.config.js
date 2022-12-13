4d1e1890915a63c16016525745303b0676b2ac0d
Dmitry Pozdnyakov <2903015@gmail.com>
2022-12-13 23:04:57 +0300

    v.0

---
+++ nuxt.config.js
@@ -0,0 +1,40 @@
+export default {
+  //target: 'static',
+
+  head: {
+    title: "Cifra",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Cifra" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [],
+    css: []
+  },
+
+  css: [],
+
+  script: [],
+
+  components: true,
+
+  buildModules: [],
+
+  modules: [
+    "@nuxtjs/toast",
+    "@nuxtjs/vuetify",
+    "@nuxtjs/universal-storage",
+  ],
+
+  toast: {
+    position: "top-right",
+    duration: 5000,
+    closeOnClick: false,
+  },
+
+  server: {
+    port: 3000,
+  },
+
+};
